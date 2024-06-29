93b24a07ed6ccc72c4bbd9abb9282087d9205432
sortegam <sortegam@users.noreply.github.com>
2024-06-29 17:31:21 +0200

    chore: Update

--- start-dev.sh
+++ start-dev.sh
@@ -2,9 +2,9 @@
 
 
 # Create SSH Keys
-if [ ! -f ./docker/nginx/ca/server.crt ] || [ ! -f ./docker/nginx/ca/server.key ]; then
-    openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 \
-        -nodes -keyout ./docker/nginx/ca/server.key -out ./docker/nginx/ca/server.crt -subj "/CN=localhost"
-fi
+# if [ ! -f ./docker/nginx/ca/server.crt ] || [ ! -f ./docker/nginx/ca/server.key ]; then
+#     openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 \
+#         -nodes -keyout ./docker/nginx/ca/server.key -out ./docker/nginx/ca/server.crt -subj "/CN=localhost"
+# fi
 
 docker compose -f docker/docker-compose.yml up
